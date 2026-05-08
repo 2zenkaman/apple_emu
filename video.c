473b17cd98505ea2943a94862748a3d71719b97f
Evgeniy <zhsliborskii@gmail.com>
2026-05-08 23:03:46 +0300

    Use original base address calculation routine instead of some handmade shit

--- video.c
+++ video.c
@@ -5,6 +5,27 @@
 extern uint8_t MEM[MEMSIZ];
 extern uint8_t factor;
 
+// routine borrowed from original autostart rom
+uint16_t bascalc(uint8_t A) {
+                uint8_t bas[2];
+                uint8_t carry = A & 1;
+/*BASCALC:*/    uint8_t temp = A;       // pha
+                A >>= 1;                // lsr A
+                A &= 0x3;               // and #$03
+                A |= 0x4;               // ora #$04
+                bas[1] = A;             // sta BASH
+                A = temp;               // pla
+                A &= 0x18;              // and #$18
+                if (carry)              // bcc BASCLC2
+                    A += 0x7f + carry;  // adc #$7f
+/*BASCLC2*/     bas[0] = A;             // sta BASL
+                A <<= 2;                // asl A
+                                        // asl A
+                bas[0] |= A;            // ora BASL
+                                        // sta BASL
+                return *(uint16_t*)bas; // rts
+}
+
 void renderchar(uint16_t x, uint16_t y, uint8_t c) {
     if (c < 32 || c > 95) return;
 
@@ -22,14 +43,10 @@ void display() {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
 
-    uint8_t* rows[] = {MEM + 0x400, MEM + 0x480, MEM + 0x500, MEM + 0x580, MEM + 0x600, MEM + 0x680, MEM + 0x700, MEM + 0x780};
-
     for (size_t y = 0; y < 24; y++) {
-        uint8_t lnum = y % 8;
-        uint8_t offset = y / 8 * 0x28;
-        uint8_t* segment = rows[lnum] + offset;
+        uint16_t bas = bascalc(y);
         for (size_t x = 0; x < 40; x++) {
-            char curr = segment[x] & 0x7F; // ASCII
+            char curr = MEM[bas + x] & 0x7F; // ASCII
             renderchar(x, y, curr);
         }
     }
